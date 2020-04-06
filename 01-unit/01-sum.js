084d08a46091e1e681d02655700106a1d146708e
Manal solanki <manalsolanki@Manals-MacBook-Air.local>
2020-04-06 12:54:56 -0400

    Implemented test cases for sum operation.

--- 01-unit/01-sum.js
+++ 01-unit/01-sum.js
@@ -1,8 +1,10 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function sum() {
-
+function sum(num1 ,num2) {
+  if (num1 == undefined) num1 = 0;
+  if (num2 == undefined) num2 = 0;
+  return Number(num1)+ Number(num2);
 }
 
 // ==================================================
@@ -20,16 +22,28 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should ignore additional numbers.
-
+  var result = sum(1, 1, 1, 1, 1, 1, 2);
+  if (result !== 2) throw new Error('Expected sum(1, 1, 1, 1, 1, 1, 2) to be 2. Received: ' + result);
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
+  var result = sum (1);
+  if (result !== 1) throw new Error(' Expected sum (1) to be 1. Recieved: ' + result);
 
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = sum ( );
+  if (result !== 0) throw new Error(' Expected sum ( ) to be 0 . Recieved: ' + result);
+
+  // --------------------------------------------------
+  // Test Case 5
+  // --------------------------------------------------
+  // When invoked a number with a string , it should return sum.
+  var result = sum ("1", "1");
+  if (result !== 2) throw new Error(' Expected sum ("1" , "1" ) to be 2 . Recieved: ' + result);
 
   console.log('All tests passed successfully.');
 

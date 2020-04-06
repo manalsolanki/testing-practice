a96446b7fd700843f195654e0c8769d5cfd57835
Manal solanki <manalsolanki@Manals-MacBook-Air.local>
2020-04-06 13:13:20 -0400

    Implemented different test cases for difference operation.

--- 01-unit/02-difference.js
+++ 01-unit/02-difference.js
@@ -1,7 +1,10 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function difference() {
+function difference(num1 , num2) {
+  if (!num1) num1 = 0;
+  if (!num2) num2 = 0;
+  return Number(num1)- Number(num2);
 
 }
 
@@ -20,16 +23,29 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should ignore additional numbers.
-
+  var result = difference(1, 1, 1, 1, 1, 1, 2);
+  if (result !== 0) throw new Error('Expected difference(1, 1, 1, 1, 1, 1, 2) to be 0. Received: ' + result);
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
+  var result = difference(1);
+  if (result !== 1) throw new Error(' Expected difference(1) to be 1. Recieved: ' + result);
 
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = difference ( );
+  if (result !== 0) throw new Error(' Expected difference ( ) to be 0 . Recieved: ' + result);
+
+  // --------------------------------------------------
+  // Test Case 5
+  // --------------------------------------------------
+  // When invoked a number with a string , it should return sum.
+  var result = difference ("1", "1");
+  if (result !== 0) throw new Error(' Expected difference("1" , "1" ) to be 0 . Recieved: ' + result);
+
 
   console.log('All tests passed successfully.');
 
